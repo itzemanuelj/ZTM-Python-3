7cd67337ea68b1c719a100b3c0f8b9aa6b36551b
Emanuel Joness <itzemanuelj@gmail.com>
2023-04-21 12:23:05 -0400

    mvp commit Python Introduction
    ztm Python Introduction

---
+++ 2 Python Introduction/introduction.py
@@ -0,0 +1,24 @@
+## Python Introduction
+
+## 5 what is a programming language
+## a way to tell the computer what to do
+
+## python file to interpretr into bytes into cpython vm which rubs the machine code
+
+## 7 how to run python code 
+
+## terminal wrap or iterm 2
+## code editors vs code
+## ide pycharm
+## notebooks jupyter
+
+## 9 our first python code
+
+name = 'justin'
+print(name)
+
+name = input ('enter your name')
+print(f'hello {name}')
+
+
+
