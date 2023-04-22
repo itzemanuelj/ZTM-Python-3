10d86f70a389be0c679aa587776da67b22a7bc7f
Emanuel Joness <itzemanuelj@gmail.com>
2023-04-22 14:40:16 -0400

    updated section 2

--- 3 Python Baics/Python Basics.py
+++ 3 Python Baics/Python Basics.py
@@ -1,2 +1,84 @@
 ## Python Basics
 
+## Fundamental Data Types
+
+## int
+nums = [1,2,3,2]
+print(type(2 + 1))
+print(type(2 / 2))
+print(type(2 - 1))
+print(2 ** 2) ## 4
+print(2 // 4)
+print(5 % 4)
+
+user_iq = 190
+print(user_iq) ## binding
+age = user_iq / 2
+a = age
+
+## float
+print(type(0.1000))
+print(type(9.9 + 1.1))
+
+x,y,z = 1,2,3
+print(x,y,z)
+
+## bool
+
+## str
+
+## list
+
+## dict 
+
+## tuple
+
+## set
+
+## dict
+
+## CLasses => custom types
+
+## specialize Data Types from modules
+
+## none
+
+## math functions
+
+print(round(3.1))
+print(round(3.9))
+
+print(abs(-20)) ## absolute values
+
+## operator precednce
+## PEMAS
+
+(20 - 3 * 4)
+12 - 20 + 8
+## () 
+## **
+## * /
+## + -
+
+
+## 45.0
+print(5.0 * 2.0 + 35)## 45.0
+## 45.0
+## 25.0
+## 25
+
+## constant
+PI = 3.14
+
+## dunder
+__hihi = 'hi'
+
+## augmented assigment operaor
+some_value = 5
+some_value = some_value + 2
+print(some_value)
+
+some_value += 2 ## shot handed way 
+
+another_value = 5
+another_value += 1
